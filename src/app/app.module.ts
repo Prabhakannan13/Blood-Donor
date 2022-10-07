096342c6f6dcff06851873d177dc1dfdd403643c
Prabhakannan13 <prabhasanthiya2001@gmail.com>
2022-10-07 21:24:29 +0530

    push2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SearchdonorComponent } from './searchdonor/searchdonor.component';
+import { AdddonorComponent } from './adddonor/adddonor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    SearchdonorComponent
+    SearchdonorComponent,
+    AdddonorComponent
   ],
   imports: [
     BrowserModule,
